ec91434bd467a8e5a27cd9bcf0cd6d73a643f545
Chhabi Sharma <chhabisharma@MacBookPro.lan>
2025-09-22 11:49:36 -0400

    trigger: force backend deployment workflow

--- src/main/java/com/care/ride/CareRideApplication.java
+++ src/main/java/com/care/ride/CareRideApplication.java
@@ -2,3 +2,4 @@ package com.care.ride;
 import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CareRideApplication{public static void main(String[] args){SpringApplication.run(CareRideApplication.class,args);}}
+// Trigger deployment Mon Sep 22 11:49:25 EDT 2025
